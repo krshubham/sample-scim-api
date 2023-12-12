400d29f326c2f309b33d8879fe3e3bd09d90c86f
Kumar Shubham <kumarshubham@acceldata.io>
2023-12-12 10:49:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+var logger = require('morgan');
+
+const scimRouter = require('./routes/scim');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  console.log(req.body);
+  if (req.headers.authorization == null) {
+    res.status(401).send("Not allowed without basic auth").end();
+  } else {
+    const val = req.headers.authorization.split(" ");
+    const encodedCredentials = val[1];
+    const [username, password] = Buffer.from(encodedCredentials, "base64").toString().split(':')
+    if (username !== password) {
+      res.status(401).end("Wrong auth");
+    }
+  }
+  next();
+})
+
+app.use('/scim/v2', scimRouter);
+
+module.exports = app;
---
+++ routes/scim.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/Users', (req, res) => {
+  res.json({
+    "schemas": ["urn:ietf:params:scim:api:messages:2.0:ListResponse"],
+    "totalResults": 0,
+    "startIndex": 1,
+    "itemsPerPage": 0,
+    "Resources": []
+  })
+});
+router.get('/Groups', (req, res) => {
+  res.json({
+    "schemas": ["urn:ietf:params:scim:api:messages:2.0:ListResponse"],
+    "totalResults": 0,
+    "startIndex": 1,
+    "itemsPerPage": 0,
+    "Resources": []
+  })
+});
+
+router.get('/Users/:userId', function(req, res, next) {
+  res.json({
+    "schemas": ["urn:ietf:params:scim:schemas:core:2.0:User"],
+    "id": "23a35c27-23d3-4c03-b4c5-6443c09e7173",
+    "userName": "test.user@okta.local",
+    "name": {
+      "givenName": "Test",
+      "middleName": "",
+      "familyName": "User"
+    },
+    "active": true,
+    "emails": [{
+      "primary": true,
+      "value": "test.user@okta.local",
+      "type": "work",
+      "display": "test.user@okta.local"
+    }],
+    "groups": [],
+    "meta": {
+      "resourceType": "User"
+    }
+  });
+});
+
+module.exports = router;
